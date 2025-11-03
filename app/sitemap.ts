0cd68899cfa9aa25991b72beb35bfbc83c0cd1b0
Aidan <aidan@p0ntus.com>
2025-11-03 13:46:19 -0500

    fix: update sitemap, usage

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -27,6 +27,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9
     },
     {
+      url: 'https://aidan.so/status',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8
+    },
+    {
+      url: 'https://aidan.so/docs',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8
+    },
+    {
       url: 'https://aidan.so/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
